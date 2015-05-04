3bd4f79005780716bdd51d1a80a4f0dbc74b17b7
Yohei Okubo <okubo.yohei@gmail.com>
2015-05-04 13:51:30 +0900

    text-game

--- src/text-game/game.lisp
+++ src/text-game/game.lisp
@@ -58,8 +58,9 @@
 ((GARDEN WEST DOOR) (ATTIC UPSTAIRS LADDAR))
 
 次に、得られたエッジからその描写を得る。
-> (mapcar #'describe-path '((garden west door) (attic upstairs laddar))
-((THERE IS A DOOR GOING WEST FROM HERE.))
+> (mapcar #'describe-path '((garden west door) (attic upstairs laddar)))
+((THERE IS A DOOR GOING WEST FROM HERE.)
+ (THERE IS A LADDAR GOING UPSTAIRS FROM HERE.))
 
 - mapcar関数
 引数に他の関数とリストを受け取って、
@@ -73,9 +74,35 @@ Lispリーダは、この記号を含む式を評価すると、内部的に次�
 >>>> (mapcar (function car) '((foo baz) (baz qux)))
 (foo baz)
 
-|#
+☆☆
+common lispでは、関数を値として扱う場合にfunctionオペレータを使ってそのことを明治しないとならない。
+関数と変数で名前が衝突した場合にエラーを引き起こす場合があるから。
+schemeは名前空間一緒。-> LISP1
 
+- 描写を結合する
+mapcarによって全てのエッジの記述のリストが得られたら、それを一つの描写にする必要がある。
+いくつかのリストをつなげて一つのリストにするappend関数を使えばそれが実現できそうだ。
+
+> (append '(mary had) '(a) '(little bomb))
+(MARY HAD A LITTLE BOMB)
+
+apped関数を使えば、通り道の描写のリストを一気に全体の描写へまtもめてしまうことができる。
+しかし、appendには、くっつけたいリストを一つづづ、別々のリストとして渡さなければならない。
+
+そこで
+- apply関数
+apply関数に関数とリストを渡すと、あたかもそのリストの各要素を引数として関数を呼び出したかのように動作する。
+
+> (apply #'append '((mary had) (a) (little bomb)))
+(MARY HAD A LITTLE BOMB)
+
+----
+desciribe-pathでみたような高階関数を多用するスタイルはLISPで頻繁に用いられる。
+
+|#
 ;;---------------------------------------------------------------------------------------
+
+;; ## 特定の場所にあるオブジェクトを描写する
 ;; オブジェクトのリスト
 (defparameter *objects* '(whiskey bucket frog chain))
 
@@ -84,18 +111,27 @@ Lispリーダは、この記号を含む式を評価すると、内部的に次�
 				   (bucket living-room)
 				   (chain garden)
 				   (frog garden)))
-
 ;; 与えられた場所から見えるオブジェクトのリストを返す関数
 (defun objects-at (loc objs obj-locs)
-  (labels ((at-loc-p (obj)
+  (labels ((at-loc-p (obj) ;; 局所関数
 	     (eq (cadr (assoc obj obj-locs)) loc)))
     (remove-if-not #'at-loc-p objs)))
+
+;; 2: labelコマンドを用いてat-loc-pという局所関数を定義
+;; 3: at-loc-p関数はオブジェクトの名前を表すシンボルを取り、それが場所locにあるかどうかを t か nil で返す。
+;; -> 関数がnilか真かの値を帰す場合、関数の名前の最後にpをつける、という慣習がある。
+;;    数値5が奇数かどうかを調べるのは、(oddp 5)という具合。
+;;    真偽値を確かめる関数は述語(predicate)と呼ばれるから。
+;; 4: remove-if-not カンスは、渡されたリストの各要素に第一引数の関数を適用し、それが真の値を返さなかったものを除いたリストを得もの。 at-loc-pが真を返すような要素だけを選び出すフィルタみたいなもん。
 
 ;;ある場所で見えるオブジェクト
 (defun describe-objects (loc objs obj-loc)
   (labels ((describe-obj (obj)
 	     `(you see a ,obj on the floor.)))
     (apply #'append (mapcar #'describe-obj (objects-at loc objs obj-loc)))))
+;; 2: 局所関数describe-obj関数を作っている
+;; 3: この関数は与えられたオブジェクトが床にある、という文を、準クォートを使って創りだす。
+;; 4: そして関数の本体では、現在の場所にある尾ぬジェクトをobjects-at関数を使って見つけ、そのオブジェクトのリストに対してdescribe-objをマップして、最後にappendで全ての描写をつなげて一つのリストにしている。
 
 
 
